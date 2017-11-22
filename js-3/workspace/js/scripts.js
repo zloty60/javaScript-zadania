2c3274138e2a3c3c4905a8fb28bcebdb0e7c3bc0
Przemysław Bzówka <przemek.bzowka@gmail.com>
2017-11-22 04:46:10 +0100

    add files

---
+++ js-3/workspace/js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt("podaj a"),
+    h= prompt("podaj h"),
+    triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
