e0376f5676a190093321c8f6ebdb42bb4e6abba8
Przemysław Bzówka <przemek.bzowka@gmail.com>
2017-12-01 20:16:43 +0100

    add file

--- js-2.6/js.js
+++ js-2.6/js.js
@@ -2,7 +2,7 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 add.addEventListener('click', function() {
   var element = document.createElement('li');
-  var x = document.getElementsByTagName('li').length
+  var x = list.getElementsByTagName('li').length;
   element.innerHTML = 'item ' + x;
   list.appendChild(element);
 });
