71739a52a4baa04b0fb74c123837b810a9e844a5
Przemysław Bzówka <przemek.bzowka@gmail.com>
2017-12-06 18:10:52 +0100

    add file

--- jquery/js.js
+++ jquery/js.js
@@ -1,14 +1,11 @@
-var span = $("span");
-    span.each(function(index, element) {
-        $("span:even").css('color', 'red');
-});
+$("span:even").css('color', 'red');
 
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+    $(element).append(button);
 });
 
 $("button").click(function(){
-	alert($(this).attr("data-tmp"));
+    alert($(this).attr("data-tmp"));
 });
