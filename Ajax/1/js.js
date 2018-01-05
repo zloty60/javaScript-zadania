382d534dbf588da0113b36ede88b55dd893e3c24
zloty60 <przemek.bzowka@gmail.com>
2018-01-05 02:56:00 +0100

    Create js.js

---
+++ Ajax/1/js.js
@@ -0,0 +1,19 @@
+var url = 'http://api.icndb.com/jokes/random';
+getJoke();
+
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function(){
+  getJoke();
+});
+
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.addEventListener('load', function(){
+    var response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+  });
+  xhr.send();
+}
